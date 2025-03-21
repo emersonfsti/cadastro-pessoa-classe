265c18bffd5f2e883cb74882aad05d6942844796
emersonfsti <emersonfsti@gmail.com>
2025-03-21 12:24:04 -0300

    commit-js001

---
+++ script.js
@@ -0,0 +1,28 @@
+class Pessoa{
+    nome;
+    idade;
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    descrever(){
+        return `Nome: ${this.nome};Idade: ${this.idade};`;
+    }
+}
+
+const form = document.getElementById('formPessoa');
+const resultado = document.getElementById('resultado');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+  
+    const pessoa = new Pessoa(nome, idade);
+    resultado.textContent = pessoa.descrever();
+  
+    form.reset();
+  });
